cfa0906c76a5c6e0ece0d37ab92cd615bbba52f4
mkirsh123 <142982085+mkirsh123@users.noreply.github.com>
2026-01-18 16:15:51 +0530

    Refactor AuthMiddleware to use token verification

--- server/Middlewares/AuthMiddleware.js
+++ server/Middlewares/AuthMiddleware.js
@@ -2,18 +2,26 @@ const User = require("../Models/UserModel");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-module.exports.userVerification = (req, res) => {
+module.exports.verifyToken = (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
-    return res.json({ status: false });
+    return res.status(401).json({ message: "Unauthorized" });
   }
-  jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
-    if (err) {
-      return res.json({ status: false });
-    } else {
-      const user = await User.findById(data.id);
-      if (user) return res.json({ status: true, user: user.username });
-      else return res.json({ status: false });
+
+  try {
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    req.user = decoded; // { id, role }
+    next();
+  } catch {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports.authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Access denied" });
     }
-  });
+    next();
+  };
 };
